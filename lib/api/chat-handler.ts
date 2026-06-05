b43e889b00019960143455468f9628ec741ee6d4
str0zzz <arakkalstrozzz@gmail.com>
2026-06-05 22:18:44 +0530

    Update chat-handler.ts

--- lib/api/chat-handler.ts
+++ lib/api/chat-handler.ts
@@ -63,7 +63,7 @@ import {
   estimatePreflightInputTokens,
   getRetryFallbackModel,
 } from "@/lib/api/chat-stream-helpers";
-import { geolocation } from "@vercelfunctions";
+import { geolocation } from "@vercel/functions";
 import { NextRequest } from "next/server";
 import {
   handleInitialChatAndUserMessage,
